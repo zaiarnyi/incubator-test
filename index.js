b41d11672c4313a8006240377f05f19e7d6cf315
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-11 18:02:41 +0200

    freecodecamp new task is done

--- index.js
+++ index.js
@@ -178,3 +178,19 @@ function processArg(num) {
 
 // Only change code below this line
 processed = processArg(7)
+
+function nextInLine(arr, item) {
+  // Only change code below this line
+
+  arr.push(item);
+  return arr.shift();
+  // Only change code above this line
+}
+
+// Setup
+let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
