0873ee2a36490eff92dc91ca019b25fac8ad0fcc
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-11 17:41:57 +0200

    freecodecamp new task is done

--- index.js
+++ index.js
@@ -75,3 +75,10 @@ const myArray = [["John", 23], ["dog", 3]];
 
 // Only change code below this line
 const removedFromMyArray = myArray.shift();
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
