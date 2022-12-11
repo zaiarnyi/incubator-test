28869204b0ec28ff8192e74c8ffeac1cdc62921d
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-11 17:32:16 +0200

    freecodecamp new task is done

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ let myStr = "Jello World";
 // Only change code below this line
 myStr = "Hello World"; // Change this line
 // Only change code above this line
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
