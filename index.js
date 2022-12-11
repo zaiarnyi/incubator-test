91638b6989a184fd0667ec971a254dac96b1d862
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-11 17:38:18 +0200

    freecodecamp new task is done

--- index.js
+++ index.js
@@ -49,3 +49,12 @@ const myArray = [18, 64, 99];
 myArray[0] = 45;
 myArray[1] = 64;
 myArray[2] = 99;
+
+const myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+const myData = myArray[2][1];
