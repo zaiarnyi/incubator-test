93fb12c9265f263e4bebaae7f04591088e866ec0
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-11 17:53:53 +0200

    freecodecamp new task is done

--- index.js
+++ index.js
@@ -168,3 +168,13 @@ function addFive(){
 
 addThree();
 addFive();
+
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7)
