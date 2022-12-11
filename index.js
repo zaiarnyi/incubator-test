49fe1c02a1316f3989eb513c6cfde7d1dc4a3e4c
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-11 17:48:50 +0200

    freecodecamp new task is done

--- index.js
+++ index.js
@@ -102,3 +102,25 @@ function timesFive(num){
 }
 
 timesFive(5);
+
+// Declare the myGlobal variable below this line
+const myGlobal = 10;
+const oopsGlobal = 5;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+
+}
+
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
