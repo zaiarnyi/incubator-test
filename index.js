a97895899323bf314c8ea3b5d04cc1cdc4ae79ce
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-11 17:33:36 +0200

    freecodecamp new task is done

--- index.js
+++ index.js
@@ -17,3 +17,9 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
