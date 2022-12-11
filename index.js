84fbb56cb9189cf7497ac8279c7a2aedcca6c262
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-11 17:34:55 +0200

    freecodecamp new task is done

--- index.js
+++ index.js
@@ -23,3 +23,12 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = [myNoun, myAdjective, myVerb, myAdverb].join(','); // Change this line
+// Only change code above this line
