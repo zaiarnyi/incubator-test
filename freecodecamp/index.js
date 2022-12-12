513f8894cf029c06ced6498dd104eef7bd1f9965
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-12 20:35:09 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -319,3 +319,16 @@ function testLessOrEqual(val) {
 }
 
 testLessOrEqual(10);
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val >= 25 && val <= 50) {
+    return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
