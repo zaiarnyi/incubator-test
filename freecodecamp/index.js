13ab4251e2f18c07a07d93b802b1cb2aadfc9890
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-12 20:30:42 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -243,3 +243,13 @@ function compareEquality(a, b) {
 }
 
 compareEquality(10, "10");
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
