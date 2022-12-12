9107b545aaf5fb565ef236785ceab39668080627
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-12 21:05:10 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -534,3 +534,11 @@ function chainToSwitch(val) {
 }
 
 chainToSwitch(7);
+
+function isLess(a, b) {
+  // Only change code below this line
+  return  a < b
+  // Only change code above this line
+}
+
+isLess(10, 15);
