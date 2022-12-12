f5604729a4be1524e80e125d03d990a28a6544ab
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-12 20:27:13 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -223,3 +223,13 @@ function testEqual(val) {
 }
 
 testEqual(10);
+
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
