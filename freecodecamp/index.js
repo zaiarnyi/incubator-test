e29c42ce8c5c3af0b63206e767f38b2fd5db757e
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-18 15:01:08 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -783,3 +783,13 @@ while( i >= 0){
 
 // Only change code below this line
 
+// Setup
+const myArray = [];
+
+for(let i = 1; i <= 5; i++){
+  myArray.push(i);
+}
+
+// Only change code below this line
+
+
