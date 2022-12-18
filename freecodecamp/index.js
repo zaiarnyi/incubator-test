9ab44c431608bfc663dac0a2a813315397a701a4
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-18 18:06:32 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1078,3 +1078,13 @@ function makeList(arr) {
 
 const failuresList = makeList(result.failure);
 
+const createPerson = (name, age, gender) => {
+  // Only change code below this line
+  return {
+    name,
+    age,
+    gender
+  };
+  // Only change code above this line
+};
+
