3e4c6f8cc48dda6a98c05e7a12fbab981a60cd3f
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-18 15:58:23 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -913,3 +913,10 @@ function randomWholeNum() {
   return Math.floor(Math.random() * 10);
 }
 
+function randomRange(myMin, myMax) {
+  // Only change code below this line
+
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+  // Only change code above this line
+}
+
