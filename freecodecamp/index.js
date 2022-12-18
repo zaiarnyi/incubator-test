855607964b4bec0ca859fe6f32a5ec3a60d20177
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-18 17:56:38 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1023,3 +1023,14 @@ const {today, tomorrow} = HIGH_TEMPERATURES
 
 // Only change code above this line
 
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+// Only change code below this line
+const {today: {low:lowToday, high: highToday}} = LOCAL_FORECAST
+
+// Only change code above this line
+
