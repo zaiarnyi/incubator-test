d4678ab653c14b30c655dbf2d0d876b2297acae8
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-18 18:00:50 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1049,3 +1049,16 @@ function removeFirstTwo(list) {
 const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const sourceWithoutFirstTwo = removeFirstTwo(source);
 
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+// Only change code below this line
+const half = ({min, max}) => (max + min) / 2.0;
+// Only change code above this line
+
