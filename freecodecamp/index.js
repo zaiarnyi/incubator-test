a6195383092849faef6c527b2a891f734ee16401
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-18 18:14:48 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1130,3 +1130,12 @@ let temp = thermos.temperature; // 24.44 in Celsius
 thermos.temperature = 26;
 temp = thermos.temperature; // 26 in Celsius
 
+<html>
+<body>
+<!-- Only change code below this line -->
+
+<!-- Only change code above this line -->
+</body>
+<script type="module" src="index.js"></script>
+</html>
+
