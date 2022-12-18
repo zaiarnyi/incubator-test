1e4eafb5d9bd039b7d1836867d36c8b44b058e32
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-18 18:07:01 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1088,3 +1088,14 @@ const createPerson = (name, age, gender) => {
   // Only change code above this line
 };
 
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
