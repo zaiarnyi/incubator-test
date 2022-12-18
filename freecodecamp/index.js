071a0ef7d8f9bdaf1aa2e9d6d596a3680c3e6b7e
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-18 14:31:39 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -674,3 +674,18 @@ function checkObj(obj, checkProp) {
   return 'Not Found'
   // Only change code above this line
 }
+
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+myMusic.push(myMusic[0])
