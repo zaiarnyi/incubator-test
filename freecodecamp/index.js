42aca44ab3ea478aa2a0adc4e1a19d2c29057d59
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-18 17:51:58 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1012,3 +1012,14 @@ arr2 = [...arr1];  // Change this line
 
 console.log(arr2);
 
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+const {today, tomorrow} = HIGH_TEMPERATURES
+
+// Only change code above this line
+
