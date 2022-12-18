1cb66d7a8f384852f67104b2232fb37c761c2b7c
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-18 18:04:00 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1062,3 +1062,19 @@ const stats = {
 const half = ({min, max}) => (max + min) / 2.0;
 // Only change code above this line
 
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  arr.forEach(item=> failureItems.push(`<li class="text-warning">${item}</li>`))
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+
