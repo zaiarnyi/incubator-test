00840fbaa7f060ee8243e371ee1b9e6131220cbc
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-18 17:59:30 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1039,3 +1039,13 @@ let a = 8, b = 6;
 
 [a, b] = [b, a];
 
+function removeFirstTwo(list) {
+  // Only change code below this line
+  const [,,...shorterList] = list; // Change this line
+  // Only change code above this line
+  return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+
