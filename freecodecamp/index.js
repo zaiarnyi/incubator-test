a209ddc0dc221ee57dbda6323e16926fd3a78b9c
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-18 15:45:05 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -843,3 +843,10 @@ do{
   i++;
 }
 
+function sum(arr, n) {
+  // Only change code below this line
+  if(!n) return 0;
+  return sum(arr, n - 1) + arr[n - 1];
+  // Only change code above this line
+}
+
