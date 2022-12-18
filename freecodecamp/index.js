76275bb4f918c1485ad7bff458daeccb3940030e
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-18 14:20:27 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -580,3 +580,14 @@ const myDog = {
 
   // Only change code above this line
 };
+
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
