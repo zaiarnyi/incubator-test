b2479f99fddcb7f4631999a6225c55b2b3909abb
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-18 14:24:56 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -633,3 +633,15 @@ const myDog = {
 };
 
 myDog.bark = 'Gav'
+
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+delete myDog.tails
