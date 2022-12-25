7b5a61156a61a9f2e82ed10010e1e709534f42df
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-25 16:32:31 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1233,6 +1233,10 @@ function rgb(r, g, b) {
   return ((1 << 24) + (detect(r) << 16) + (detect(g) << 8) + detect(b)).toString(16).slice(1).toUpperCase();
 }
 
+function howManyDalmatians(number) {
+  var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+  return number <= 10 ? dogs[0] : (number <= 50 ? dogs[1] : (number === 101 ? dogs[3] : dogs[2]));
+}
 
 
 
