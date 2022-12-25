1f3ea74b0a165dcd21874c783cc17f6eb1764d0b
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-25 16:33:50 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1237,6 +1237,8 @@ function howManyDalmatians(number) {
   var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
   return number <= 10 ? dogs[0] : (number <= 50 ? dogs[1] : (number === 101 ? dogs[3] : dogs[2]));
 }
+// SELECT * FROM companies
+// ORDER BY employees DESC
 
 
 
