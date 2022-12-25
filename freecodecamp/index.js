7438e04b48e4421d3b2fb64bd226a7ce7df3effb
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-25 23:22:37 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1524,6 +1524,19 @@ const countSmileys = arr => {
   return count;
 };
 
+const duplicateCount = text => {
+  let newText = text.toLowerCase().split("");
+  let ans = {};
+  let count = 0;
+  for (let char of newText) {
+    ans[char] ? ans[char]++ : (ans[char] = 1);
+  }
+  for (let key in ans) {
+    if (ans[key] >= 2) count++;
+  }
+  return count;
+};
+
 
 
 
