87789c4f074631063cfe0afa47e760b1428b26e3
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-26 17:17:27 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1626,6 +1626,13 @@ function mineLocation(field) {
     }
   }
 }
+const findMissing = list => {
+  const diff = (list[list.length - 1] - list[0]) / list.length;
+  for (let i = 0; i < list.length - 1; i++) {
+    if (list[i + 1] - list[i] !== diff) return list[i] + diff;
+  }
+};
+
 
 
 
