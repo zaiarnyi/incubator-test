d38871c4c84686c35bcd11a1f924868ff1bc4586
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-26 17:42:01 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1866,6 +1866,10 @@ let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/; // Change this line
 let result = extractStr.match(codingRegex); // Change this line
 
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
+
 
 
 
