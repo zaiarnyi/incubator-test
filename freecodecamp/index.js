018b2f596c76ef977beafbabbadcf77797edb14e
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-26 17:48:01 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1874,6 +1874,10 @@ let exampleStr = "Let's have fun with regular expressions!";
 let unRegex = /.un/; // Change this line
 let result = unRegex.test(exampleStr);
 
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[eaoui]/gi; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
+
 
 
 
