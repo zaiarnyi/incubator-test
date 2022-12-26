40d197e844f0327f6f58990e825bdd43d54ef6ce
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-26 17:23:43 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1736,6 +1736,16 @@ const groupByCommas = (n) => {
   );
 };
 
+function mineLocation(field) {
+  for (let i = 0; i < field.length; i++) {
+    for (let j = 0; j < field[i].length; j++) {
+      if (field[i][j] === 1) {
+        return [i, j];
+      }
+    }
+  }
+}
+
 
 
 
