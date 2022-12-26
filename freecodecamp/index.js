a2838e7e44ff38a4b1ee882e2da81b93006b98f7
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-26 17:41:01 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1858,6 +1858,14 @@ let petString = "James has a pet cat.";
 let petRegex = /(John|Emma|Liz|Alice) has a pet (dog|bird|cat|fish)/; // Change this line
 let result = petRegex.test(petString);
 
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; // Change this line
+let result = fccRegex.test(myString);
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line
+
 
 
 
