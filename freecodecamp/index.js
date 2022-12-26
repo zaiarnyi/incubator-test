46bd16e4fba4cba1b7430ce2af49f9490799817f
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-26 17:20:11 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1679,6 +1679,20 @@ const findInArray = (array, iterator) => {
   return -1;
 };
 
+const makeAmpString = names => `${names[0].name} & ${names[1].name}`;
+
+const list = names => {
+  if (names.length === 0) return "";
+  if (names.length === 1) return names[0].name;
+  if (names.length === 2) return makeAmpString(names);
+  const lastTwo = makeAmpString(names.slice(-2));
+  const others = names
+    .slice(0, -2)
+    .map(n => n.name + ",")
+    .join(" ");
+  return `${others} ${lastTwo}`;
+};
+
 
 
 
