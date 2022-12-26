83e7dfdf830e225d9a155c03d06be7bb49424a19
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-26 17:26:07 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1775,6 +1775,42 @@ const stockList = (listOfArt, listOfCat) => {
     }, "")
     .slice(0, -3);
 };
+const to_nato = words =>
+  [...words.toUpperCase()]
+    .filter(c => c !== " ")
+    .reduce((acc, curr) => (acc += `${conversion(curr)} `), "")
+    .trim();
+
+const conversion = char => (chart[char] ? chart[char] : char);
+
+const chart = {
+  A: "Alfa",
+  B: "Bravo",
+  C: "Charlie",
+  D: "Delta",
+  E: "Echo",
+  F: "Foxtrot",
+  G: "Golf",
+  H: "Hotel",
+  I: "India",
+  J: "Juliett",
+  K: "Kilo",
+  L: "Lima",
+  M: "Mike",
+  N: "November",
+  O: "Oscar",
+  P: "Papa",
+  Q: "Quebec",
+  R: "Romeo",
+  S: "Sierra",
+  T: "Tango",
+  U: "Uniform",
+  V: "Victor",
+  W: "Whiskey",
+  X: "Xray",
+  Y: "Yankee",
+  Z: "Zulu"
+};
 
 
 
