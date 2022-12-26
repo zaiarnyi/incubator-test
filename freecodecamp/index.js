8bfc8fab1d03a482f29d6db2e1f6b495b2a917e7
zaiarnyi <fl.zaiarnyi@gmail.com>
2022-12-26 17:09:04 +0200

    freecodecamp new task is done

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -1588,6 +1588,18 @@ const songDecoder = song => {
     .filter(word => word)
     .join(" ");
 };
+const duplicateEncode = word => {
+  let lcWord = word.toLowerCase();
+  let charCount = {};
+  let answer = "";
+  for (let char of lcWord) {
+    charCount[char] ? charCount[char]++ : (charCount[char] = 1);
+  }
+  for (let char of lcWord) {
+    charCount[char] > 1 ? (answer += ")") : (answer += "(");
+  }
+  return answer;
+};
 
 
 
